a79ebf0c29b850a233f0f428b0311c757b1ed0bf
Daniel Soto <danielrsoto@gmail.com>
2009-06-13 19:15:34 -0700

    initial commit of temperature converter

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TempConverter
+//
+//  Created by Daniel Soto on 13 Jun 09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
